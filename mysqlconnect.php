541a26e76e055792787d3738c03bf52ed845b693
DESKTOP-UEF4OC9\Andrew <acastelluccio@drew.edu>
2017-09-28 16:21:18 -0400

    no message

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -2,7 +2,7 @@
 $user = 'd2qpf22lyarz395l';
 $password = 'pejiin9edn8xmt5a';
 $db = 'py6etou4vck57kfy';
-$host = 'xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	';
+$host = 'xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $port = 3306;
 
 $link = mysqli_init();
