ae000652e9f94bce99e9694cd7d4f017275ce8ef
Kent Harris <kent@Kents-MacPro-2015-Laptop.local>
2017-10-18 00:06:08 -0400

    fixed an extra r in database password

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -1,6 +1,6 @@
 <?php
 $user = 'd2qpf22lyarz395l';
-$password = 'rpejiin9edn8xmt5aoot';
+$password = 'pejiin9edn8xmt5a';
 $db = 'py6etou4vck57kfy';
 $host = 'xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $port = 3306;
