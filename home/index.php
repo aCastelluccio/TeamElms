541de06c96cc7bd955c722090cbf2acfce94bd44
Camille Berger <Camille@Camilles-MBP.home>
2017-11-23 10:13:27 -0500

    Add spacing between pictures

--- home/index.php
+++ home/index.php
@@ -201,18 +201,24 @@ if (!isset($_SESSION['active_session']) || ($_SESSION['active_session'] === fals
       <div class="row">
         <div class="col-md-7">
           <a href="#">
+              
+              
+              
             <img class="img-fluid rounded mb-3 mb-md-0" src="https://drive.google.com/uc?export=view&id=<?php 
             echo $row['images_path'];?> "alt="" height="50%" width="50%" >
               
+           
           </a>
            
         </div>
           
         <div class="col-md-5">
-          <p>comment</p>
+          <p>comment </p>
           
         </div>
+          
       </div>
+          <br>
         <?php } ?>
 
     </div>
