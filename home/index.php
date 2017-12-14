a0ec749943839bef80ef2efa93bfd58959090b1f
KentHHarris <kentharris5123@yahoo.com>
2017-12-14 14:37:27 -0500

    correct gdrive href

--- home/index.php
+++ home/index.php
@@ -237,7 +237,7 @@ if (!isset($_SESSION['active_session']) || ($_SESSION['active_session'] === fals
 
                         <div class="many">
                             <!-- More Than 20 Images Upload-->
-                            <p><a href="https://drive.google.com/drive/u/1/folders/0B0Uz_T5t_1jOaWMyNmh5UURaaDQ">
+                            <p><a href="https://drive.google.com/drive/folders/1060dPAJ7b2q1haJSQvwPjCzshDQRUfbB">
                                 <?php if ($_SESSION['admin'] === true) { echo 'If you are uploading more than 20 images, upload them directly here'; } ?></a></p>
                         </div>
                          <!-- Space -->
